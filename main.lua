64a4fe4a2a26a150d7a4b141e8e79eb747d76683
D1044172058 <suzutochk1208@gmail.com>
2018-10-31 13:42:14 +0800

    Revert "D104417205"
    This reverts commit 2a1301cc224eeeb3521f4f38acbeddfbf5cff5f7.

--- main.lua
+++ main.lua
@@ -12,5 +12,3 @@ local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.s
 msgText:setFillColor( 0.2, 0.6, 0.8 )
 local msgText = display.newText( "Hello, world!", world.x, world.y+200, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
-local msgText = display.newText( "Hello, world!", world.x, world.y+240, native.systemFont, 32 )
-msgText:setFillColor( 0.2, 0.6, 0.8 )
